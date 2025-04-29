95e65ffdd1ef27d2a83414d29a0210c5dd211602
RxBLx <babussalamprojex@gmail.com>
2025-04-29 14:41:14 +0700

    Delete

--- script.js
+++
@@ -1,8 +0,0 @@
-// Trigger Animation on Scroll
-window.addEventListener('scroll', function() {
-  document.querySelectorAll('.fade-in').forEach(function(el) {
-    if (el.getBoundingClientRect().top < window.innerHeight * 0.9) {
-      el.classList.add('visible');
-    }
-  });
-});
